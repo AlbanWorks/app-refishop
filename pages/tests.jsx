8556de2e93fbd9b5287f00d510487bf6695bb0b6
Santiago Nicolás Lizardo Alba <69883681+AlbanWorks@users.noreply.github.com>
2023-12-04 23:19:31 -0300

    Update tests.jsx

--- pages/tests.jsx
+++ pages/tests.jsx
@@ -1,9 +1,9 @@
 import React,{useState,useEffect} from "react"
-import useFcmToken from "../src/hooks/useFcmToken"
+
 
 export default function Tests() {
 
-const { fcmToken, notificationPermissionStatus } = useFcmToken();
+
 
   return (
    <div>
