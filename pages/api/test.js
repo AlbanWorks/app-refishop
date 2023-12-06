3c6b8837f9058846c23c1ddc120ff0ce8fe82725
Santiago Nicolás Lizardo Alba <69883681+AlbanWorks@users.noreply.github.com>
2023-12-05 23:36:09 -0300

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,7 +2,19 @@ import { firestore } from '../../src/services/firebase/firebaseAdminConfig'
 
 const webPush = require('web-push');
 const PUBLIC_KEY = 'BPWimmkwnT81UvyNWP-HuxokPW_UA9uahnA6Twd737UgDgGkY5eIOI32O0cdHhSIKMvUWGYd0zmQ7mpABX3TNiw'
-const PRIVATE_KEY = 'zkKpFLTnaOc1KW64fpBA4uEJ8thf58oodRarE26x2u0'
+const PRIVATE_KEY = process.env.WEB_PUSH_PRIVATE_KEY
+
+const base64ToUint8Array = base64 => {
+    const padding = '='.repeat((4 - (base64.length % 4)) % 4)
+    const b64 = (base64 + padding).replace(/-/g, '+').replace(/_/g, '/')
+    const rawData = window.atob(b64)
+    const outputArray = new Uint8Array(rawData.length)
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+  }
+
 webPush.setVapidDetails('mailto:example@yourdomain.org', PUBLIC_KEY, PRIVATE_KEY)
 
 const getSubscriptionObject = async (id) =>{
