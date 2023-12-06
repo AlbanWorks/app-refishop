440dd8acee6a78036f8a62872e89df88fb5cedaf
Santiago Nicolás Lizardo Alba <69883681+AlbanWorks@users.noreply.github.com>
2023-12-05 22:50:51 -0300

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,9 +1,9 @@
 import { firestore } from '../../src/services/firebase/firebaseAdminConfig'
+const webPush = require('web-push');
 
-/*const webPush = require('web-push');
-const PUBLIC_KEY = process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY
-const PRIVATE_KEY = process.env.WEB_PUSH_PRIVATE_KEY
-webPush.setVapidDetails('mailto:example@yourdomain.org', PUBLIC_KEY, PRIVATE_KEY)*/
+const PUBLIC_KEY = 'BPWimmkwnT81UvyNWP-HuxokPW_UA9uahnA6Twd737UgDgGkY5eIOI32O0cdHhSIKMvUWGYd0zmQ7mpABX3TNiw'
+const PRIVATE_KEY = 'zkKpFLTnaOc1KW64fpBA4uEJ8thf58oodRarE26x2u0'
+webPush.setVapidDetails('mailto:example@yourdomain.org', PUBLIC_KEY, PRIVATE_KEY)
 
 const getSubscriptionObject = async (id) =>{
     try{
